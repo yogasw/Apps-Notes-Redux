2896d14c3d4e454205fcb1061058a00990640875
Yoga Setiawan <mryoga1995@gmail.com>
2019-06-25 12:25:41 +0700

    home error

--- App/Screens/Home.js
+++ App/Screens/Home.js
@@ -2,131 +2,147 @@ import React from "react";
 import {
     Container, Header, Left, Body, Right,
     Button, Title, Item, Card, CardItem,
-    Content, Input, Text,Fab,
+    Content, Input, Text, Fab,
 } from "native-base";
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {View, ScrollView} from "react-native";
 
 export default class HomeScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: 'true'
+        };
+    }
+
     render() {
         return (
             <Container>
-                <Header>
-                    <Left>
-                        <Button
-                            transparent
-                            onPress={() => this.props.navigation.navigate("DrawerOpen")}>
-                            <Icon color="white" size={25} name="menu"/>
-                        </Button>
-                    </Left>
-                    <Body>
-                        <Title>Note App</Title>
-                    </Body>
-                    <Right>
-                        <Button
-                            transparent
-                            onPress={() => this.props.navigation.navigate("DrawerOpen")}>
-                            <Icon name="sort-descending" color="white" size={25}/>
-                        </Button>
-                    </Right>
-                </Header>
-                <Item rounded style={styles.search}>
-                    <Input placeholder='Rounded Textbox'/>
-                </Item>
-                <Content padder>
-                    <View style={styles.content}>
-                        <Card>
-                            <CardItem style={styles.cardItem}>
-                                <Body>
-                                    <Text style={styles.date} right>20 Juni 2019</Text>
-                                    <Text style={styles.title}>Title</Text>
-                                    <Text style={styles.category}>Category</Text>
-                                    <Text style={styles.note}>Cur lactea moriGemnas congregabo!?</Text>
-                                </Body>
-                            </CardItem>
-                        </Card>
-                        <Card>
-                            <CardItem style={styles.cardItem}>
-                                <Body>
-                                    <Text style={styles.date} right>20 Juni 2019</Text>
-                                    <Text style={styles.title}>Title</Text>
-                                    <Text style={styles.category}>Category</Text>
-                                    <Text style={styles.note}>Danistas sunt competitio</Text>
-                                </Body>
-                            </CardItem>
-                        </Card>
-                        <Card>
-                            <CardItem style={styles.cardItem}>
-                                <Body>
-                                    <Text style={styles.date} right>20 Juni 2019</Text>
-                                    <Text style={styles.title}>Title</Text>
-                                    <Text style={styles.category}>Category</Text>
-                                    <Text style={styles.note}>Danistas sunt competitio</Text>
-                                </Body>
-                            </CardItem>
-                        </Card>
-                        <Card>
-                            <CardItem style={styles.cardItem}>
-                                <Body>
-                                    <Text style={styles.date} right>20 Juni 2019</Text>
-                                    <Text style={styles.title}>Title</Text>
-                                    <Text style={styles.category}>Category</Text>
-                                    <Text style={styles.note}>Danistas sunt competitio</Text>
-                                </Body>
-                            </CardItem>
-                        </Card>
-                        <Card>
-                            <CardItem style={styles.cardItem}>
-                                <Body>
-                                    <Text style={styles.date} right>20 Juni 2019</Text>
-                                    <Text style={styles.title}>Title</Text>
-                                    <Text style={styles.category}>Category</Text>
-                                    <Text style={styles.note}>Danistas sunt competitio</Text>
-                                </Body>
-                            </CardItem>
-                        </Card>
-                        <Card>
-                            <CardItem style={styles.cardItem}>
-                                <Body>
-                                    <Text style={styles.date} right>20 Juni 2019</Text>
-                                    <Text style={styles.title}>Title</Text>
-                                    <Text style={styles.category}>Category</Text>
-                                    <Text style={styles.note}>Danistas sunt competitio</Text>
-                                </Body>
-                            </CardItem>
-                        </Card>
-                        <Card>
-                            <CardItem style={styles.cardItem}>
-                                <Body>
-                                    <Text style={styles.date} right>20 Juni 2019</Text>
-                                    <Text style={styles.title}>Title</Text>
-                                    <Text style={styles.category}>Category</Text>
-                                    <Text style={styles.note}>Danistas sunt competitio</Text>
-                                </Body>
-                            </CardItem>
-                        </Card>
-                        <Card>
-                            <CardItem style={styles.cardItem}>
-                                <Body>
-                                    <Text style={styles.date} right>20 Juni 2019</Text>
-                                    <Text style={styles.title}>Title</Text>
-                                    <Text style={styles.category}>Category</Text>
-                                    <Text style={styles.note}>Danistas sunt competitio</Text>
-                                </Body>
-                            </CardItem>
-                        </Card>
-                        <Card>
-                            <CardItem style={styles.cardItem}>
-                                <Body>
-                                    <Text style={styles.date} right>20 Juni 2019</Text>
-                                    <Text style={styles.title}>Title</Text>
-                                    <Text style={styles.category}>Category</Text>
-                                    <Text style={styles.note}>Danistas sunt competitio</Text>
-                                </Body>
-                            </CardItem>
-                        </Card>
-                    </View>
-                </Content>
+                    <Header style={{backgroundColor: '#ffffff' }}>
+                        <Left>
+                            <Button
+                                transparent
+                                onPress={() => this.props.navigation.navigate("DrawerOpen")}>
+                                <Icon color="black" size={25} name="menu"/>
+                            </Button>
+                        </Left>
+                        <Body>
+                            <Title>Note App</Title>
+                        </Body>
+                        <Right>
+                            <Button
+                                transparent
+                                onPress={() => this.props.navigation.navigate("DrawerOpen")}>
+                                <Icon name="sort-descending" color="black" size={25}/>
+                            </Button>
+                        </Right>
+                    </Header>
+                    <Item rounded style={styles.search}>
+                        <Input placeholder='Search...'/>
+                    </Item>
+                    <Content padder>
+                        <View style={styles.content}>
+                            <Card>
+                                <CardItem style={styles.cardItem}>
+                                    <Body>
+                                        <Text style={styles.date} right>20 Juni 2019</Text>
+                                        <Text style={styles.title}>Title</Text>
+                                        <Text style={styles.category}>Category</Text>
+                                        <Text style={styles.note}>Cur lactea moriGemnas congregabo!?</Text>
+                                    </Body>
+                                </CardItem>
+                            </Card>
+                            <Card>
+                                <CardItem style={styles.cardItem}>
+                                    <Body>
+                                        <Text style={styles.date} right>20 Juni 2019</Text>
+                                        <Text style={styles.title}>Title</Text>
+                                        <Text style={styles.category}>Category</Text>
+                                        <Text style={styles.note}>Danistas sunt competitio</Text>
+                                    </Body>
+                                </CardItem>
+                            </Card>
+                            <Card>
+                                <CardItem style={styles.cardItem}>
+                                    <Body>
+                                        <Text style={styles.date} right>20 Juni 2019</Text>
+                                        <Text style={styles.title}>Title</Text>
+                                        <Text style={styles.category}>Category</Text>
+                                        <Text style={styles.note}>Danistas sunt competitio</Text>
+                                    </Body>
+                                </CardItem>
+                            </Card>
+                            <Card>
+                                <CardItem style={styles.cardItem}>
+                                    <Body>
+                                        <Text style={styles.date} right>20 Juni 2019</Text>
+                                        <Text style={styles.title}>Title</Text>
+                                        <Text style={styles.category}>Category</Text>
+                                        <Text style={styles.note}>Danistas sunt competitio</Text>
+                                    </Body>
+                                </CardItem>
+                            </Card>
+                            <Card>
+                                <CardItem style={styles.cardItem}>
+                                    <Body>
+                                        <Text style={styles.date} right>20 Juni 2019</Text>
+                                        <Text style={styles.title}>Title</Text>
+                                        <Text style={styles.category}>Category</Text>
+                                        <Text style={styles.note}>Danistas sunt competitio</Text>
+                                    </Body>
+                                </CardItem>
+                            </Card>
+                            <Card>
+                                <CardItem style={styles.cardItem}>
+                                    <Body>
+                                        <Text style={styles.date} right>20 Juni 2019</Text>
+                                        <Text style={styles.title}>Title</Text>
+                                        <Text style={styles.category}>Category</Text>
+                                        <Text style={styles.note}>Danistas sunt competitio</Text>
+                                    </Body>
+                                </CardItem>
+                            </Card>
+                            <Card>
+                                <CardItem style={styles.cardItem}>
+                                    <Body>
+                                        <Text style={styles.date} right>20 Juni 2019</Text>
+                                        <Text style={styles.title}>Title</Text>
+                                        <Text style={styles.category}>Category</Text>
+                                        <Text style={styles.note}>Danistas sunt competitio</Text>
+                                    </Body>
+                                </CardItem>
+                            </Card>
+                            <Card>
+                                <CardItem style={styles.cardItem}>
+                                    <Body>
+                                        <Text style={styles.date} right>20 Juni 2019</Text>
+                                        <Text style={styles.title}>Title</Text>
+                                        <Text style={styles.category}>Category</Text>
+                                        <Text style={styles.note}>Danistas sunt competitio</Text>
+                                    </Body>
+                                </CardItem>
+                            </Card>
+                            <Card>
+                                <CardItem style={styles.cardItem}>
+                                    <Body>
+                                        <Text style={styles.date} right>20 Juni 2019</Text>
+                                        <Text style={styles.title}>Title</Text>
+                                        <Text style={styles.category}>Category</Text>
+                                        <Text style={styles.note}>Danistas sunt competitio</Text>
+                                    </Body>
+                                </CardItem>
+                            </Card>
+                        </View>
+                    </Content>
+                <Fab
+                    active={this.state.active}
+                    direction="up"
+                    containerStyle={{ }}
+                    style={{ backgroundColor: '#ffffff' }}
+                    position="bottomRight"
+                    onPress={() => this.setState({ active: !this.state.active })}>
+                    <Icon name="plus" style={{color:'black'}}/>
+                </Fab>
             </Container>
         );
     }
@@ -139,10 +155,8 @@ const styles = {
         marginLeft: 20
     },
     content: {
-
         flexDirection: 'row',
         flexWrap: 'wrap',
-        justifyContent: 'space-between'
     },
     title: {
         fontSize: 20,
@@ -159,12 +173,11 @@ const styles = {
         marginTop: 20,
         fontSize: 17,
         color: 'white',
-
     },
     cardItem: {
         backgroundColor: colorBg(),
-        height: 160,
-        width: 160,
+        height: 145,
+        width: 145,
     },
     date: {
         fontSize: 14,
@@ -172,7 +185,6 @@ const styles = {
         alignSelf: 'flex-end'
     }
 };
-
 function colorBg() {
     let colorArray = [];
 
