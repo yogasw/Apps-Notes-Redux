cc66168d6e083f34c32671e19dc9a8db613b3510
Yoga Setiawan <mryoga1995@gmail.com>
2019-06-27 08:26:40 +0700

    reformat code

--- App/Screens/EditNote.js
+++ App/Screens/EditNote.js
@@ -1,43 +1,31 @@
 import React from "react";
 import {
-    Container, Header, Left, Body, Right, Button, Title, Content, Form, Textarea, Picker, Text
+    Container, Content, Form, Textarea, Picker, Text
 } from "native-base";
-
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import HeaderMenu from "../Components/HeaderMenu";
+import {View, Dimensions} from "react-native";
 
 export default class HomeScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            selected: "key1"
+            selected: "key1",
+            widthScrren: null,
+            heightScreen: null,
         };
+
     }
+
+    _onLayout = event => {
+        let {width, height} = event.nativeEvent.layout;
+        this.state.widthScrren = width;
+        this.state.heightScreen = height;
+        console.log(this.state.widthScrren);
+    }; // width, height, x, y
 
     render() {
         return (
-            <Container>
-                {/*<Header style={{backgroundColor: '#ffffff'}}>
-                    <Left>
-                        <Button
-                            transparent
-                            onPress={() => this.props.navigation.navigate("Home")}>
-                            <Icon color="black" size={25} name="keyboard-backspace"/>
-                        </Button>
-                    </Left>
-                    <Body>
-                        <Title style={{color: 'black'}}>Edit Note</Title>
-                    </Body>
-                    <Right>
-                        <Button
-                            transparent
-                            onPress={() => {
-                                this.props.navigation.navigate('Home')
-                            }}>
-                            <Icon name="check-circle-outline" color="#3DB39E" size={25}/>
-                        </Button>
-                    </Right>
-                </Header>*/}
+            <Container onLayout={this._onLayout}>
                 <HeaderMenu
                     leftPress={() => this.props.navigation.navigate("Home")}
                     leftIcon="keyboard-backspace"
@@ -50,22 +38,24 @@ export default class HomeScreen extends React.Component {
                         <Textarea rowSpan={5} placeholder="ADD TITLE ..."/>
                         <Textarea rowSpan={5} placeholder="ADD DESCRIPTION ..."/>
                         <Text style={{color: 'black'}}>CATEGORY</Text>
-                        <Picker
-                            mode='dropdown'
-                            style={{width: 120}}
-                            selectedValue={this.state.selected}>
-                            <Picker.Item label="Wallet" value="key0"/>
-                            <Picker.Item label="Wallet" value="key1"/>
-                            <Picker.Item label="Wallet" value="key2"/>
-                            <Picker.Item label="Wallet" value="key3"/>
-                            <Picker.Item label="Wallet" value="key4"/>
-                            <Picker.Item label="Wallet" value="key5"/>
-                            <Picker.Item label="Wallet" value="key5"/>
-                        </Picker>
+                        <View style={{width: this.state.widthScrren}}>
+                            <Picker
+                                mode='dropdown'
+                                selectedValue={this.state.selected}>
+                                <Picker.Item label="Wallet" value="key0"/>
+                                <Picker.Item label="Wallet" value="key1"/>
+                                <Picker.Item label="Wallet" value="key2"/>
+                                <Picker.Item label="Wallet" value="key3"/>
+                                <Picker.Item label="Wallet" value="key4"/>
+                                <Picker.Item label="Wallet" value="key5"/>
+                                <Picker.Item label="Wallet" value="key5"/>
+                            </Picker>
+                        </View>
                     </Form>
                 </Content>
             </Container>
         );
     }
 }
+
 
